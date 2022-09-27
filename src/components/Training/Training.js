9d3accbdd217f3e797ddbd073d6f4b1146cb3178
IemetsO <oksana.v.iemets@gmail.com>
2022-09-27 21:28:18 +0300

    Training

--- src/components/Training/Training.js
+++ src/components/Training/Training.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import { useState } from 'react';
-import { DateInput } from "@blueprintjs/datetime";
+import React from "react";
 import "../Training/Training.css";
 
-
-
-export default function Training() {
-  const [date, setDate] = useState('');
-
-  const handleDateChange = e => {
-       console.log(e)
-       setDate(e);
-       
-  }
-  console.log(date)
-  console.log(DateInput)
-  return (
-      <DateInput
-    formatDate={date => date.toLocaleString()}
-    parseDate={str => new Date(str)}
-    highlightCurrentDay = "true"
-    onChange={handleDateChange}
-    
-       
+import { DateRangeInput2 } from "@blueprintjs/datetime2";
+import { useCallback, useState } from "react";
+import { isTomorrow } from "date-fns";
+ 
+function Training() {
+    const [dateValue, setDateValue] = useState("");
+    const handleChange = useCallback(setDateValue, []);
+    const formatDate = useCallback((date) => date.toDateString(), []);
+    const parseDate = useCallback((str) => new Date(str), []);   
+   
+    return (
+        <DateRangeInput2
+            formatDate={formatDate}
+            onChange={handleChange}
+            parseDate={parseDate}
+            placeholder="M/D/YYYY"
+            value={dateValue}
+            allowSingleDayRange = "true"
+            highlightCurrentDay = "true"
+            minDate={new Date()} 
         />
-     
-
-  );
+    );
 }
+
+export default Training
