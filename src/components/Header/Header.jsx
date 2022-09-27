c0467135e658d232572f1b16347c326ea818d547
Ratushnyy Andrey <90136453+Oper992@users.noreply.github.com>
2022-09-27 15:05:59 +0300

    Хедер готовий

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,37 @@
 import { Link, Outlet } from "react-router-dom";
 import { HandySvg } from "handy-svg";
-import iconHome from "../../icon/icon_home.svg";
-import iconLibrary from "../../icon/icon_library.svg";
+import iconHome from "../../img/icon home.svg";
+import iconLibrary from "../../img/icon library.svg";
 import style from "./Header.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import { logout } from "../../redux/operations";
+// import { useDispatch, useSelector } from "react-redux";
+import { useMediaQuery } from "react-responsive";
+
+const Desktop = ({ children }) => {
+  const isDesktop = useMediaQuery({ minWidth: 1280 });
+  return isDesktop ? children : null;
+};
+const Tablet = ({ children }) => {
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1279 });
+  return isTablet ? children : null;
+};
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  return isMobile ? children : null;
+};
 
 export const Header = () => {
-  const dispatch = useDispatch();
-  const userName = useSelector((state) => state.booksReading.userName);
-  const isLogin = useSelector((state) => state.booksReading.isLogin);
+  //   const dispatch = useDispatch();
+  //   Те що повинно бути у стейті редакса
+  //   const userName = useSelector((state) => state.booksReading.userName);
+  //   const isLogin = useSelector((state) => state.booksReading.isLogin);
+
+  // Для прикладу роботи логіки
+  const isLogin = true;
+  const userName = "Володимир Зеленський";
 
   const handleLogout = () => {
-    dispatch(logout());
+    // потрібно добавити апі та налаштувати редакс
+    // dispatch();
   };
 
   const getFirstLetterOfName = (userName) => {
@@ -21,30 +40,86 @@ export const Header = () => {
 
   return (
     <>
-      <div className={style.header}>
-        {true ? (
-          <>
-            <p className={style.logoBeforeLogin}>BR</p>
-            <Link to="/library" className={style.iconLibrary}>
-              <HandySvg src={iconLibrary} width="22" height="17" />
-            </Link>
-            <Link to="/training" className={style.iconHome}>
-              <HandySvg src={iconHome} width="20" height="17" />
-            </Link>
-            <p className={style.avatar}>{getFirstLetterOfName("Андрій")}</p>
-            <button
-              className={style.logout}
-              type="button"
-              onClick={handleLogout}
-            >
-              Вихід
-            </button>
-          </>
-        ) : (
-          <p className={style.logoAfterLogin}>BR</p>
-        )}
-      </div>
-      <Outlet />
+      <Mobile>
+        <div className={style.header}>
+          {isLogin ? (
+            <>
+              <p className={style.logoBeforeLogin}>BR</p>
+              <Link to="/library" className={style.iconLibrary}>
+                <HandySvg src={iconLibrary} width="22" height="17" />
+              </Link>
+              <Link to="/training" className={style.iconHome}>
+                <HandySvg src={iconHome} width="20" height="17" />
+              </Link>
+              <p className={style.avatar}>{getFirstLetterOfName(userName)}</p>
+              <button
+                className={style.logout}
+                type="button"
+                onClick={handleLogout}
+              >
+                Вихід
+              </button>
+            </>
+          ) : (
+            <p className={style.logoAfterLogin}>BR</p>
+          )}
+          <Outlet />
+        </div>
+      </Mobile>
+      <Tablet>
+        <div className={style.header}>
+          {isLogin ? (
+            <>
+              <p className={style.logoBeforeLogin}>BR</p>
+              <p className={style.avatar}>{getFirstLetterOfName(userName)}</p>
+              <p className={style.userName}>{userName}</p>
+              <Link to="/library" className={style.iconLibrary}>
+                <HandySvg src={iconLibrary} width="22" height="17" />
+              </Link>
+              <Link to="/training" className={style.iconHome}>
+                <HandySvg src={iconHome} width="20" height="17" />
+              </Link>
+              <button
+                className={style.logout}
+                type="button"
+                onClick={handleLogout}
+              >
+                Вихід
+              </button>
+            </>
+          ) : (
+            <p className={style.logoAfterLogin}>BR</p>
+          )}
+          <Outlet />
+        </div>
+      </Tablet>
+      <Desktop>
+        <div className={style.header}>
+          {isLogin ? (
+            <>
+              <p className={style.logoBeforeLogin}>BR</p>
+              <p className={style.avatar}>{getFirstLetterOfName(userName)}</p>
+              <p className={style.userName}>{userName}</p>
+              <Link to="/library" className={style.iconLibrary}>
+                <HandySvg src={iconLibrary} width="22" height="17" />
+              </Link>
+              <Link to="/training" className={style.iconHome}>
+                <HandySvg src={iconHome} width="20" height="17" />
+              </Link>
+              <button
+                className={style.logout}
+                type="button"
+                onClick={handleLogout}
+              >
+                Вихід
+              </button>
+            </>
+          ) : (
+            <p className={style.logoAfterLogin}>BR</p>
+          )}
+          <Outlet />
+        </div>
+      </Desktop>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
@@ -11,9 +10,9 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <BrowserRouter>
       <App />
-    </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
