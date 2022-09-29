2a77ee4a58d449f95af1d9045c632a8ae2d89b4a
Helarios <kuzinaksenya32@gmail.com>
2022-09-29 22:28:29 +0300

    add auth logic

---
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -0,0 +1,8 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { userSelector } from "../../redux/selector/user-selector";
+
+export const PublicRoute = ({ children, navigateTo }) => {
+  const isLogin = useSelector(userSelector.getIsLogin);
+  return !isLogin ? children : <Navigate to={navigateTo} />;
+};
---
+++ src/components/UserInfoInput/index.js
@@ -0,0 +1,1 @@
+export { UserInfoInput } from "./UserInfoInput";
