36da4c7f55a4ce2b3c1bab80d0084b27427ba223
kesel <a.kiselyoff@gmail.com>
2022-10-06 03:03:42 +0300

    have a problem with styled-components

--- src/pages/Statistics/StatisticsPage.jsx
+++ src/pages/Statistics/StatisticsPage.jsx
@@ -1,18 +1,36 @@
+import styled from "styled-components";
 import ShowTimer from "../../components/Timer/ShowTimer/ShowTimer";
 import MyTarget from "../../components/MyTarget/MyTarget";
-import Training from "../../components/Training/Training";
+// import Training from "../../components/Training/Training";
 import s from "./StatisticsPage.module.css";
 import StatisticsTable from "../../components/StatisticsTable/StatisticsTable";
 import StatisticsBook from "../../components/StatisticsBook/StatisticsBook";
+import { Chart } from "../../components/Chart/Chart";
+
+const ShowTimerStyled = styled(ShowTimer)`
+  margin-bottom: 40px;
+  background-color: red;
+  color: red;
+  height: 600px;
+`;
+
 const StatisticsPage = () => {
+  // const Container = styled.div`
+  //   margin-bottom: 40px;
+  //   background-color: red;
+  //   color: red;
+  //   height: 600px;
+  // `;
+
   return (
     <>
-      <div className={s.container}>
-        <ShowTimer />
+      <div className={s.containerPage}>
+        <ShowTimerStyled />
         <MyTarget />
-        <Training />
-        <StatisticsTable />
         <StatisticsBook />
+        <Chart />
+        {/* <Training /> */}
+        <StatisticsTable />
       </div>
     </>
   );
