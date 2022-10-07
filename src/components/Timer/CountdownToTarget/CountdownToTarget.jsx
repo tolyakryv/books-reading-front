507e2f725f5ac541aedb27d98b5e214f9a77546f
kesel <a.kiselyoff@gmail.com>
2022-10-07 03:57:01 +0300

    add query but not work

--- src/components/Timer/CountdownToTarget/CountdownToTarget.jsx
+++ src/components/Timer/CountdownToTarget/CountdownToTarget.jsx
@@ -1,3 +1,4 @@
+import { toast } from "react-toastify";
 import useCountdown from "../../../hooks/useCountdown";
 import ShowCounter from "../ShowCounter/ShowCounter";
 import styles from "./CountdownToTarget.module.css";
@@ -6,6 +7,10 @@ const CountdownToTarget = ({ startTime, endTime }) => {
   const [days, hours, minutes, seconds] = useCountdown(startTime, endTime);
   if (days + hours + minutes + seconds <= 0) {
     console.log("countdown is over");
+    toast.info(
+      "Ти молодчина, але потрібно швидше! Наступного разу тобі все вдасться)"
+    );
+
     return (
       <div className={styles.countdownContainer}>
         <p className={styles.caption}>До досягнення мети залишилось</p>
--- src/components/Timer/ShowTimer/ShowTimer.jsx
+++ src/components/Timer/ShowTimer/ShowTimer.jsx
@@ -1,16 +1,34 @@
+import { useEffect, useState } from "react";
 import CountdownToEndYear from "../CountdownToEndYear/CountdownToEndYear";
 import CountdownToTarget from "../CountdownToTarget/CountdownToTarget";
 import getEndOfYear from "../getEndOfYear";
 import s from "./ShowTimer.module.css";
+import { useGetTrainQuery } from "../../../services/trainingAPI";
 
-const startTime = new Date("2022-09-28T19:39:30").getTime();
-const endTime = new Date("2022-09-28T19:39:35").getTime();
+// const startTime = new Date("2022-09-28T19:39:30").getTime();
+// const endTime = new Date("2022-09-28T19:39:35").getTime();
 const now = Date.now();
 // const now = new Date("2022-12-31T23:59:50").getTime();
 // const endOfYear = new Date(2022, 11, 32);
 const endOfYear = getEndOfYear(new Date().getFullYear());
 
 const ShowTimer = ({ className }) => {
+  const [startTime, setStartTime] = useState(0);
+  const [endTime, setEndTime] = useState(0);
+  const { data } = useGetTrainQuery();
+
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+      const startTime = data.startDate;
+      setStartTime(startTime);
+      console.log(startTime);
+      const endTime = data.finishDate;
+      setEndTime(endTime);
+      console.log(endTime);
+    }
+  }, [data]);
+
   return (
     <div className={`${s.timerContainer} ${className}`}>
       <CountdownToEndYear startTime={now} endTime={endOfYear} />
