576200139eaaf079be8f0319f1961291afa36336
Helarios <kuzinaksenya32@gmail.com>
2022-10-08 20:58:56 +0300

    add statistics counter

--- src/pages/Statistics/StatisticsPage.jsx
+++ src/pages/Statistics/StatisticsPage.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useEffect, useState } from "react";
 import { Mobile, Tablet, Desktop } from "../../helpers/responsiveComponents";
 import ShowTimer from "../../components/Timer/ShowTimer/ShowTimer";
 import MyTarget from "../../components/MyTarget/MyTarget";
@@ -6,6 +7,13 @@ import s from "./StatisticsPage.module.css";
 import StatisticsTable from "../../components/StatisticsTable/StatisticsTable";
 import StatisticsBook from "../../components/StatisticsBook/StatisticsBook";
 import { Chart } from "../../components/Chart/Chart";
+import {
+  useAddTrainStatisticMutation,
+  useDelTrainMutation,
+  useGetTrainQuery,
+  useUpdateStatusBookMutation,
+} from "../../services/trainingAPI";
+import { format } from "date-fns";
 
 const ShowTimerStyled = styled(ShowTimer)`
   margin-bottom: 40px;
@@ -21,13 +29,81 @@ const StatisticsBookStyled = styled(StatisticsBook)`
 // `;
 
 const StatisticsPage = () => {
+  const { data } = useGetTrainQuery();
+  const [updateStatusBook] = useUpdateStatusBookMutation();
+  const [addTrainStatistics] = useAddTrainStatisticMutation();
+  const [deleteTrain] = useDelTrainMutation();
+  const [recentlyReadPages, setRecentlyReadPages] = useState(0);
+
+  const onReadBook = async (amountPages) => {
+    try {
+      await addTrainStatistics({
+        formalizedDate: format(new Date(), "dd.MM.yyyy"),
+        pageNumber: amountPages - recentlyReadPages,
+        createdAt: new Date().toLocaleTimeString(),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const checkAlreadyReadBooks = async (books, statistic) => {
+      try {
+        const alreadyReadBooks = books.filter(
+          (book) => book.status === "alreadyRead"
+        );
+        const countPagesOfAlreadyReadBooks = alreadyReadBooks.reduce(
+          (acc, book) => acc + book.amountPages,
+          0
+        );
+        const ReadingNowBooks = books.filter(
+          (book) => book.status === "readingNow"
+        );
+
+        let countReadPages = statistic.reduce(
+          (acc, book) => acc + book.amountPages,
+          0
+        );
+
+        setRecentlyReadPages(countReadPages - countPagesOfAlreadyReadBooks);
+
+        const firstBook = ReadingNowBooks[0];
+
+        if (recentlyReadPages < firstBook.amountPages) return;
+
+        const bookId = firstBook._id;
+        const status = "alreadyRead";
+        await updateStatusBook({
+          bookId,
+          status,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (!data) return;
+
+    const { book, statistic } = data;
+
+    const readingNowBooks = book.filter((book) => book.status === "readingNow");
+    if (readingNowBooks.length === 0) {
+      console.log("Delete Train");
+      deleteTrain();
+      return;
+    }
+
+    checkAlreadyReadBooks(book, statistic);
+  }, [data, deleteTrain, recentlyReadPages, updateStatusBook]);
+
   return (
     <>
       <Mobile>
         <div className={s.containerPage}>
           <ShowTimerStyled />
           <MyTargetStyled />
-          <StatisticsBookStyled />
+          <StatisticsBookStyled onReadBook={onReadBook} />
           <Chart />
           <StatisticsTable />
         </div>
@@ -36,7 +112,7 @@ const StatisticsPage = () => {
         <div className={s.containerPage}>
           <ShowTimerStyled />
           <MyTargetStyled />
-          <StatisticsBookStyled />
+          <StatisticsBookStyled onReadBook={onReadBook} />
           <Chart />
           <StatisticsTable />
         </div>
@@ -46,7 +122,7 @@ const StatisticsPage = () => {
           <div className={s.wrapperRow}>
             <div className={s.wrapperCol}>
               <ShowTimer />
-              <StatisticsBook />
+              <StatisticsBook onReadBook={onReadBook} />
             </div>
             <MyTarget />
           </div>
