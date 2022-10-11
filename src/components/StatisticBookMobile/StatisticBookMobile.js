c808e60a47bfab549dbb783867ff624627dce3cd
IemetsO <oksana.v.iemets@gmail.com>
2022-10-11 14:54:11 +0300

    fix bug

--- src/components/StatisticBookMobile/StatisticBookMobile.js
+++ src/components/StatisticBookMobile/StatisticBookMobile.js
@@ -88,24 +88,26 @@ const StatisticBookMobile = ({ onReadBook }) => {
               </label>
             </div>
             <div className={s.bigColumn}>
-              <div className={s.title}><span className={s.width}>{e.title}</span></div>
-              <div className={s.row}>
-                <div className={s.column}>
-                  <div className={s.cell}>
-                    <span className={s.grey}>Автор:</span>
+            <div className={s.box}>
+                <p className={s.width}><span className={s.title}>{e.title}</span></p>
+                <div className={s.row}>
+                  <div className={s.column}>
+                    <dl className={s.description}>
+                      <dt className={s.key}>Автор:</dt>
+                      <dd className={s.property}>
+                        <span className={s.author}>{e.author}</span>
+                      </dd>
+                    </dl>
+                    <dl className={s.description}>
+                      <dt className={s.key}>Рік:</dt>
+                      <dd className={s.property}>{e.publicDate}</dd>
+                    </dl>
+                    <dl className={s.description}>
+                      <dt className={s.key}>Стор.:</dt>
+                      <dd className={s.property}>{e.amountPages}</dd>
+                    </dl>
                   </div>
-                  <div className={s.cell}>
-                    <span className={s.grey}>Рік:</span>
                   </div>
-                  <div className={s.cell}>
-                    <span className={s.grey}>Стор.:</span>
-                  </div>
-                </div>
-                <div className={s.column}>
-                  <div className={s.cell}>{e.author}</div>
-                  <div className={s.cell}>{e.publicDate}</div>
-                  <div className={s.cell}>{e.amountPages}</div>
-                </div>
               </div>
             </div>
           </div>
